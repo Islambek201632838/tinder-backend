74733cf59b93cce8aa98c1a48583e5030c3c2bbc
Isevich_savage <124886919+Islambek201632838@users.noreply.github.com>
2024-03-24 18:28:13 +0500

    Add files via upload

--- src/main/java/com/discord/backend/profileService/controller/ProfileController.java
+++ src/main/java/com/discord/backend/profileService/controller/ProfileController.java
@@ -5,10 +5,8 @@ import com.discord.backend.profileService.service.ProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/profiles")
@@ -21,9 +19,26 @@ public class ProfileController {
         this.profileService = profileService;
     }
 
+    @GetMapping
+    public ResponseEntity<List<Profile>> getAllProfiles() {
+        List<Profile> profiles = profileService.getAllProfiles();
+        return ResponseEntity.ok(profiles);
+    }
+
     @PostMapping
     public ResponseEntity<Profile> createProfile(@RequestBody Profile profile) {
-        Profile createdProfile = profileService.createProfile(profile);
+        Profile createdProfile = profileService.saveOrUpdateProfile(profile);
         return new ResponseEntity<>(createdProfile, HttpStatus.CREATED);
+    }
+    @PutMapping("/updateUniversity/{id}")
+    public ResponseEntity<Profile> updateUniversity(@PathVariable String id, @RequestBody String university) {
+        Profile updatedProfile = profileService.updateUniversity(id, university);
+        return new ResponseEntity<>(updatedProfile, HttpStatus.OK);
+    }
+
+    @PutMapping("/updateBuddy/{id}")
+    public ResponseEntity<Profile> updateBuddy(@PathVariable String id, @RequestBody String buddy) {
+        Profile updatedProfile = profileService.updateBuddy(id, buddy);
+        return new ResponseEntity<>(updatedProfile, HttpStatus.OK);
     }
 }
